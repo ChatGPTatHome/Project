d71136277c63be7af00132da171548e925dd0239
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-04-03 01:57:13 -0700

    pls merge error

--- Test.java
+++ Test.java
@@ -1,6 +1,5 @@
-
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("accept me please");
+    public static void
+        System.out.println("accept dsadasd please");
     }
 }
