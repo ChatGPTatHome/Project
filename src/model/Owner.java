9bb9470bb87e24038117012b753b28d8d9a3da69
Jeremiah <Breni.jeremiah.2021@gmail.com>
2024-04-26 16:55:46 -0700

    Java doc Owner and ProfileScreen

--- src/model/Owner.java
+++ src/model/Owner.java
@@ -1,12 +1,35 @@
 package model;
 
+/**
+ * @author Jeremiah Brenio.
+ *
+ * @version v1.00
+ * 
+ *          Stores the Owner's information.
+ */
 public class Owner {
 
+    /** The Owner's name. */
+    private String name;
+
+    /** The Owner's email. */
+    private String email;
+
+    /** Sets the owner's to N/A by default. */
+    public Owner() {
+        name = "N/A";
+        email = "N/A";
+    }
+
+    /** Sets the Owner's information. */
     public void setOwner(String name, String email) {
-
+        this.name = name;
+        this.email = email;
     }
 
-    public void getOwner(String name, String email) {
-
+    /** Returns the Owner's information. */
+    public String getOwner() {
+        return name + "" + email;
     }
+
 }
--- src/view/ProfileScreen.java
+++ src/view/ProfileScreen.java
@@ -1,9 +1,34 @@
 package view;
 
-public class ProfileScreen {
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-    public void openProfile() {
+import model.Owner;
 
+/**
+ * @author Jeremiah Brenio.
+ *
+ * @version v1.00
+ * 
+ *          Displays a JPanel to set ownership of the App.
+ */
+public class ProfileScreen extends CardPanel {
+
+    /** To store the owner's name and email address. */
+    private Owner owner;
+
+    /** */
+    private JTextField nameText;
+
+    /** Makes a ProfileScreen with text boxes. */
+    public ProfileScreen() {
+        super();
+        owner = new Owner();
+
+    }
+
+    public String getName() {
+        return "ProfileScreen";
     }
 
 }
