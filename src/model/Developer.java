697612695ac6038cb41146a4a61ebe67a83f4f52
Hoai Thuong Le <hoaithuongle@Hoais-MBP.lan>
2024-04-26 17:48:16 -0700

    move developer and add return

--- src/model/Developer.java
+++ src/model/Developer.java
@@ -1,20 +1,20 @@
-package view;
+package model;
 
 public class Developer {
 
     public String getAnthony() {
-
+        return "Anthony";
     }
 
     public String getHai() {
-
+        return "Hai";
     }
 
     public String getJeremiah() {
-
+        return "Jeremiah";
     }
 
     public String getWindie() {
-
+        return "Windie";
     }
 }
