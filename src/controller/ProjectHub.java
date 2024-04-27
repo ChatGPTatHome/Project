2bbdd80fe6a5b4441594be33467ca8782edcd815
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-04-26 17:41:41 -0700

    create owner in main and accept owner in home constructor

--- src/controller/ProjectHub.java
+++ src/controller/ProjectHub.java
@@ -2,21 +2,24 @@ package controller;
 
 import view.MainScreen;
 import view.HomeScreen;
+import model.Owner;
 import view.AboutScreen;
+import view.Developer;
 import view.ProfileScreen;
 
 public class ProjectHub {
+    private static final Owner owner = new Owner();
 
     public static void main(String[] theArgs) {
         MainScreen window = new MainScreen();
 
-        HomeScreen home = new HomeScreen();
+        HomeScreen home = new HomeScreen(owner);
         AboutScreen about = new AboutScreen();
-        // ProfileScreen profile = new ProfileScreen();
+        ProfileScreen profile = new ProfileScreen(owner);
 
         window.addCard(home);
         window.addCard(about);
-        // window.addCard(profile);
+        window.addCard(profile);
 
         window.showCard(home);
 
