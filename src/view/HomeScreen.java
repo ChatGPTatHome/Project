b9b9d2ee2d4572d8b7a8ce4b7fb25142a62e0534
Hai Duong <149478749+HaixD@users.noreply.github.com>
2024-04-26 17:19:52 -0700

    dw its just updates

--- src/view/HomeScreen.java
+++ src/view/HomeScreen.java
@@ -1,7 +1,41 @@
 package view;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
 public class HomeScreen extends CardPanel {
-    public HomeScreen() {}
+    public HomeScreen() {
+        this.setLayout(new BorderLayout());
+        
+        JPanel home = new JPanel();
+        home.setLayout(new BoxLayout(home, BoxLayout.Y_AXIS));
+
+        JPanel entries = new JPanel();
+        entries.setLayout(new BoxLayout(entries, BoxLayout.X_AXIS));
+
+        JPanel nameEntry = new JPanel();
+        nameEntry.add(new JLabel("Name:"));
+        nameEntry.add(new JTextField(10));
+
+        JPanel emailEntry = new JPanel();
+        emailEntry.add(new JLabel("email:"));
+        emailEntry.add(new JTextField(10));
+
+        entries.add(nameEntry);
+        entries.add(emailEntry);
+
+        home.add(entries);
+        home.add(new JButton("Submit"));
+
+        this.add(home, BorderLayout.CENTER);
+    }
 
     public String getName() {
         return "Home";
