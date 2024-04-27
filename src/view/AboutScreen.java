7351de014aeaddc276416b8a2fad131d9b351b8b
Anthony Chapkin <anthonyvchapkin@gmail.com>
2024-04-26 17:23:43 -0700

    Have AboutScreen extend card panel

--- src/view/AboutScreen.java
+++ src/view/AboutScreen.java
@@ -1,21 +1,43 @@
 package view;
 
+import model.About;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Is a JPanel. Shows information about the app.
  */
-public class AboutScreen extends JPanel {
+public class AboutScreen extends CardPanel {
+
+    private static final String NAME = "About";
 
     /**
-     * Constructs this AboutScreen Panel.
+     * Constructs this AboutScreen Panel. Calls multiple
+     * methods to set up this panel.
      */
-    AboutScreen() {
-        
+    public AboutScreen() {
+        setLayout();
+        setText();
     }
 
+    /**
+     * Getter for the name of this screen.
+     *
+     * @return the name "About".
+     */
+    public String getName() {
+        return NAME;
+    }
 
+    private void setText() {
+        JLabel label = new JLabel("Version: V" + About.getVersion());
+        add(label);
+    }
 
+    private void setLayout() {
+        setLayout(new FlowLayout());
+    }
 
 
 }
