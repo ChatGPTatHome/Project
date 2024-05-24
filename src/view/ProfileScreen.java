81543528bbe58d6ed4d3f4f5582370d4b8b30da1
Anthony Chapkin <anthonyvchapkin@gmail.com>
2024-05-24 11:51:08 -0700

    Recover ProfileScreen, and add it back to view

---
+++ src/view/ProfileScreen.java
@@ -0,0 +1,56 @@
+package view;
+
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+
+import model.ImportExport;
+import model.Models;
+
+/**
+ * @author Jeremiah Brenio, Hai Duong.
+ *
+ * @version v1.00
+ *
+ *          Displays a JPanel to display ownership of the App.
+ */
+public class ProfileScreen extends Screen {
+
+    private ImportExport importExport;
+
+    /** To show owner's name. */
+    private JLabel nameLabel;
+
+    /** To show owner's email. */
+    private JLabel emailLabel;
+
+    /** Makes a ProfileScreen with text boxes. */
+    public ProfileScreen(Models modelSource) {
+        super(modelSource);
+        this.importExport = this.getModel(ImportExport.class);
+        nameLabel = new JLabel("NAME: " + importExport.getName());
+        emailLabel = new JLabel("EMAIL: " + importExport.getEmail());
+
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        add(new JLabel("Owner: "));
+        add(nameLabel);
+        add(emailLabel);
+    }
+
+    /**
+     * Returns the name of the Screen.
+     */
+    @Override
+    public String getName() {
+        return "Profile";
+    }
+
+    /**
+     * Updates the Owner's information.
+     */
+    @Override
+    public void update() {
+        nameLabel.setText(importExport.getName());
+        emailLabel.setText(importExport.getEmail());
+        importExport.updateSettings();
+    }
+}
