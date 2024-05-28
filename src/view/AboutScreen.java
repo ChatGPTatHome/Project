d3078917e6c92215346087eba696a1979fed0fe0
Anthony Chapkin <anthonyvchapkin@gmail.com>
2024-05-27 21:44:35 -0700

    Add JavaDocs to AboutScreen

--- src/view/AboutScreen.java
+++ src/view/AboutScreen.java
@@ -1,8 +1,6 @@
 package view;
 
 import model.About;
-import model.Developer;
-import model.ImportExport;
 import model.Models;
 
 import javax.swing.*;
@@ -15,12 +13,24 @@ import java.awt.*;
  */
 public class AboutScreen extends Screen {
 
+    /**
+     * Constant that represents this classes
+     * name as a String.
+     */
     private static final String NAME = "About";
+
+    /**
+     * Model class used to read persistent data
+     * about this app.
+     */
     private final About about;
 
     /**
-     * Constructs this AboutScreen Panel. Calls multiple
-     * methods to set up this panel.
+     * Constructs this AboutScreen. Calls multiple
+     * methods to set up this Screen.
+     *
+     * @param models A Models object used to initialize
+     *               models used by this AboutScreen.
      */
     public AboutScreen(Models models) {
         this.about = models.getModel(About.class);
@@ -30,9 +40,9 @@ public class AboutScreen extends Screen {
     }
 
     /**
-     * Getter for the name of this screen.
+     * Getter for the name of this Screen.
      *
-     * @return the name "About".
+     * @return the String constant field NAME.
      */
     @Override
     public String getName() {
@@ -40,13 +50,18 @@ public class AboutScreen extends Screen {
     }
 
     /**
-     * Does nothing, just overrides update.
+     * Implements and overrides Screen's update()
+     * method. Has no functionality.
      */
     @Override
     public void update() {
 
     }
 
+    /**
+     * Sets up the GUI for this AboutScreen. Uses About model
+     * to fetch persistent data.
+     */
     private void setUpPanel() {
         JPanel panel1 = new JPanel();
         panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
