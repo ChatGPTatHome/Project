751f0eb5be9c2d4ced900048039a73d6710b5d3b
Jeremiah <brenio.jeremiah.2021@gmail.com>
2024-06-06 07:09:39 -0700

    Add JavaDoc and Improve display of Owner information

--- src/view/ProfileScreen.java
+++ src/view/ProfileScreen.java
@@ -1,6 +1,10 @@
 package view;
 
-import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.Box;
 import javax.swing.JLabel;
 
 import model.ImportExport;
@@ -15,6 +19,7 @@ import model.Models;
  */
 public class ProfileScreen extends Screen {
 
+    /** ImportExport model to retrieve persistent data. */
     private ImportExport importExport;
 
     /** To show owner's name. */
@@ -23,21 +28,51 @@ public class ProfileScreen extends Screen {
     /** To show owner's email. */
     private JLabel emailLabel;
 
-    /** Makes a ProfileScreen with text boxes. */
+    /**
+     * Constructor for ProfileScreen.
+     * Displays the owner's name and email.
+     *
+     * @param modelSource The model source to retrieve data.
+     * @author Jeremiah Brenio - Created GUI and functionality to display owner's
+     *         name and email.
+     *         Hai Duong - Added compatability with modelSource to access
+     *         ImportExport.class.
+     */
     public ProfileScreen(Models modelSource) {
         super(modelSource);
         this.importExport = this.getModel(ImportExport.class);
+        JLabel ownerLabel = new JLabel("Owner");
         nameLabel = new JLabel("NAME: " + importExport.getName());
         emailLabel = new JLabel("EMAIL: " + importExport.getEmail());
 
-        setLayout(new FlowLayout(FlowLayout.CENTER));
-        add(new JLabel("Owner: "));
-        add(nameLabel);
-        add(emailLabel);
+        Font ownerFont = new Font("Arial", Font.BOLD, 30);
+        Font labelFont = new Font("Arial", Font.PLAIN, 20);
+        ownerLabel.setFont(ownerFont);
+        nameLabel.setFont(labelFont);
+        emailLabel.setFont(labelFont);
+
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbcOwner = new GridBagConstraints();
+        gbcOwner.gridwidth = GridBagConstraints.REMAINDER;
+        gbcOwner.anchor = GridBagConstraints.NORTH;
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weighty = 1;
+
+        add(ownerLabel, gbcOwner);
+        add(Box.createVerticalGlue(), gbc);
+        add(nameLabel, gbc);
+        add(emailLabel, gbc);
+        add(Box.createVerticalGlue(), gbc);
     }
 
     /**
      * Returns the name of the Screen.
+     * 
+     * @author Hai Duong
      */
     @Override
     public String getName() {
@@ -46,11 +81,13 @@ public class ProfileScreen extends Screen {
 
     /**
      * Updates the Owner's information.
+     * 
+     * @author Jeremiah Brenio
      */
     @Override
     public void update() {
-        nameLabel.setText(importExport.getName());
-        emailLabel.setText(importExport.getEmail());
+        nameLabel.setText("NAME: " + importExport.getName());
+        emailLabel.setText("EMAIL: " + importExport.getEmail());
         importExport.updateSettings();
     }
 }
